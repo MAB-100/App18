e8b59e69422c54b707ab9f4c0ef6e80c5c6ec7a0
MAB-100 <shahabbas661@gmail.com>
2024-08-02 19:30:18 +0500

    created db model

--- resturant_menu/models.py
+++ resturant_menu/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+MEAL_TYPE = (
+    ('starters', 'starters'),
+    ('salads', 'salads'),
+    ('main_dishes', 'main_dishes'),
+    ('desserts', 'desserts'),
+)
+
+STATUS = (
+    (1, 'available'),
+    (0, 'not_available'),
+)
+
+class Item(models.Model):
+    meal = models.CharField(max_length=1000, unique=True)
+    description = models. CharField(max_length=2000)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    meal_type = models.CharField(choices=MEAL_TYPE, max_length=200)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    staus = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.meal
+    
+
